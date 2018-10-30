eb37b65a51cdb7ad30882c352b70e19d26cf969c
itzrene <renko.krcmar12@gmail.com>
2018-10-30 15:35:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
-client.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    client.commands.set(props.help.name, props);
-  });
-
-})
 
 var leaveMessages = [
   "didn't really like it here :(",
