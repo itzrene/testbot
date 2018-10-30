c415848547c742618dc3194e89843bfa7149ed7f
itzrene <renko.krcmar12@gmail.com>
2018-10-30 17:02:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on("message", (message) => {
   .setThumbnail("http://iconbug.com/data/95/256/8696325e0e7407823058632e68fb5970.png")
   .setTitle("Results:")
   .addField(args, fortunes[result], true)
-  message.channel.send({embed: embed});
+  message.channel.send({embed})
 
   }
 
