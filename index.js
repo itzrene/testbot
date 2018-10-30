9294a0835251cf1dbaadfc30926c2a70721e0763
itzrene <renko.krcmar12@gmail.com>
2018-10-30 14:33:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var leaveMessages = [
+  "didn't really like it here :(",
+  "has left, bye",
+  "what a.. nevermind!",
+  "has left the server!",
+  "has left, maybe they will come back.."
+];
+
 client.on("message", function(message) {
   if (message.content === "ping") {
     message.reply("pong");
@@ -9,16 +17,16 @@ client.on("message", function(message) {
 
 client.on("message", function(message) {
   if (message.content === "hello") {
-    message.channel.send("bye " + message.author.toString());
+    message.channel.send("Hello! <3 " + message.author.toString());
   }
 });
 
 client.on("guildMemberAdd", member => {
-  member.guild.channels.get("455016389556568084").send("**" + member.user.toString() + "** has joined the server!");
+  member.guild.channels.get("455016389556568084").send(member.user.toString() + " welcome to **ᴄ ʜ ɪ ʟ ʟ    ᴄ ᴏ ᴜ ɴ ᴛ ʏ**! Make sure to check out #welcome for any information, or ask a staff member! <3 Enjoy your stay!");
 });
 
 client.on("guildMemberRemove", member => {
-  member.guild.channels.get("455016389556568084").send("**" + member.user.toString() + "** has left the server!");
+  member.guild.channels.get("455016389556568084").send("**" + member.user.username + "** " + leaveMessages);
 });
 
 client.login(process.env.BOT_TOKEN);
