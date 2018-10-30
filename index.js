58e8682cb16ab3cddbdc322de34c77c790733942
itzrene <renko.krcmar12@gmail.com>
2018-10-30 16:55:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("message", (message) => {
   .setColor(0x00AE86)
   .setThumbnail("http://iconbug.com/data/95/256/8696325e0e7407823058632e68fb5970.png")
   .setTitle("Results:")
-  .addField(args, fortunes[result])
+  .addField(args, fortunes[result], true)
   message.channel.send({embed});
 
   }
