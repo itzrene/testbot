485187aadd798d5ab88c8a0d605e66312111d8ed
itzrene <renko.krcmar12@gmail.com>
2018-10-30 14:56:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("message", function(message) {
 });
 
 var messageJoin = member.user.toString() + " welcome to **ᴄ ʜ ɪ ʟ ʟ    ᴄ ᴏ ᴜ ɴ ᴛ ʏ**! Make sure to check out " +
-  message.guild.channels.get('455022347225792532').toString() + " for any information, or ask a staff member! 💓 Enjoy your stay!");
+  message.guild.channels.get('455022347225792532').toString() + " for any information, or ask a staff member! 💓 Enjoy your stay!";
 
 client.on("guildMemberAdd", member => {
   member.guild.channels.get("455016389556568084").send(messageJoin);
