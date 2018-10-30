c532fbb7d5c0bf0e9995c9b1f18318fbd45574a9
itzrene <renko.krcmar12@gmail.com>
2018-10-30 17:23:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ client.on("message", (message) => {
   .setColor(0xb798f2)
   .setThumbnail("http://iconbug.com/data/95/256/8696325e0e7407823058632e68fb5970.png")
   .setTitle("Results:")
-  .addField(args, " - ")
-  .addField(" - ", fortunes[result]);
+  .addField(" ", args)
+  .addField(" ", fortunes[result]);
   message.channel.send({embed});
 
   }
