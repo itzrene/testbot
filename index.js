6bc3f0ce3b9f495cd7a56b4059cde7dca642dce8
itzrene <renko.krcmar12@gmail.com>
2018-10-31 03:01:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,111 +1,46 @@
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = "!"
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
-if(cmd === `${prefix}report`){
+fs.readdir("./commands/", (err, files) => {
 
-  module.exports.run = async (bot, message, args) => {
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("Couldn't find user.");
-      let rreason = args.join(" ").slice(22);
+  if(err) console.log(err);
 
-      let reportEmbed = new Discord.RichEmbed()
-      .setDescription("Reports")
-      .setColor("#15f153")
-      .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-      .addField("Channel", message.channel)
-      .addField("Time", message.createdAt)
-      .addField("Reason", rreason);
-
-      let reportschannel = message.guild.channels.find(`name`, "reports");
-      if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-
-
-      message.delete().catch(O_o=>{});
-      reportschannel.send(reportEmbed);
-
-      return;
-
-    }
-  };
-
-var leaveMessages = [
-  "didn't really like it here :(",
-  "has left, bye",
-  "what a.. nevermind!",
-  "has left the server!",
-  "has left, maybe they will come back.."
-];
-
-client.on("message", function(message) {
-  if (message.content === "hello") {
-    message.channel.send("Hello! 💓 " + message.author.toString());
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
   }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
 });
 
-client.on("message", function(message) {
-  if (message.content === "hi") {
-    message.channel.send("Hello! 💓 " + message.author.toString());
-  }
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+
+  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+
+  //bot.user.setGame("on SourceCade!");
 });
 
-client.on("message", function(message) {
-  if (message.content === "test") {
-    message.channel.send("ok " + message.author.toString());
-  }
-});
-
-client.on("message", (message) => {
-
+bot.on("message", async message => {
   if(message.author.bot) return;
-  if(!message.content.startsWith(prefix)) return;
+  if(message.channel.type === "dm") return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
-  if(command === "8ball") {
-    var fortunes = [
-    "Yes",
-    "No",
-    "Maybe",
-    "Definitely!",
-    "Ask me later..",
-    "Fuck u",
-    "I'm not sure",
-    "Probably",
-    "I doubt..",
-    "Who? Me?",
-    "Sorry, I'm too lazy to answer",
-    "I thought you know the answer"
-    ];
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
-    var result = Math.floor((Math.random() * fortunes.length) + 0);
-
-    const embed = new Discord.RichEmbed()
-  .setColor(0xb798f2)
-  .setThumbnail("http://iconbug.com/data/95/256/8696325e0e7407823058632e68fb5970.png")
-  .setTitle("Results:")
-  .addField("Q:", args)
-  .addField("A:", fortunes[result]);
-  message.channel.send({embed});
-
-  }
-
-});
-
-client.on("guildMemberAdd", member => {
-  member.guild.channels.get("506563742352277507").send(member.user.toString() + " welcome to **ᴄ ʜ ɪ ʟ ʟ    ᴄ ᴏ ᴜ ɴ ᴛ ʏ**! Make sure to check out #welcome for any information, or ask a staff member! 💓 Enjoy your stay!");
-});
-
-client.on("guildMemberRemove", member => {
-  member.guild.channels.get("506551891841253406").send("**" + member.user.username + "** " + leaveMessages[Math.floor(Math.random() * leaveMessages.length)]);
-});
-
-client.on("ready", () => {
-
-  client.user.setGame("| 𝒶𝑒𝓈𝓉𝒽𝑒𝓉𝒾𝒸")
-
-});
-
-client.login(process.env.BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN);
