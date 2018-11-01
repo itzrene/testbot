afb451fa328aaec07c49973a498f349bc0d607fa
itzrene <renko.krcmar12@gmail.com>
2018-11-02 00:46:58 +0100

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -6,9 +6,9 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
-    
+
     let embed = new Discord.RichEmbed()
-    .setColor("##598267")
+    .setColor("#598267")
     .addField(`Cleared ${args[0]} messages`, "Yikes!");
 
   message.channel.send(embed).then(msg => {msg.delete(5000)});
