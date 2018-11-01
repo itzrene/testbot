7a9ab7ba7dd17fe6df078b8257f62a63650df1fd
itzrene <renko.krcmar12@gmail.com>
2018-11-01 18:24:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,83 @@
-const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-let purple = botconfig.purple;
-let cooldown = new Set();
-let cdseconds = 5;
+const client = new Discord.Client();
+const prefix = "!"
 
-fs.readdir("./commands/", (err, files) => {
+var leaveMessages = [
+  "didn't really like it here :(",
+  "has left, bye",
+  "what a.. nevermind!",
+  "has left the server!",
+  "has left, maybe they will come back.."
+];
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
+client.on("message", function(message) {
+  if (message.content === "hello") {
+    message.channel.send("Hello! 💓 " + message.author.toString());
   }
-
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
 });
 
-bot.on("ready", async () => {
-
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("| 𝒶𝑒𝓈𝓉𝒽𝑒𝓉𝒾𝒸", {type: "WATCHING"});
-
+client.on("message", function(message) {
+  if (message.content === "hi") {
+    message.channel.send("Hello! 💓 " + message.author.toString());
+  }
 });
 
+client.on("message", function(message) {
+  if (message.content === "test") {
+    message.channel.send("ok " + message.author.toString());
+  }
+});
 
-bot.on("message", async message => {
+client.on("message", (message) => {
 
   if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: botconfig.prefix
-    };
-  }
-
-  let prefix = prefixes[message.guild.id].prefixes;
   if(!message.content.startsWith(prefix)) return;
-  if(cooldown.has(message.author.id)){
-    message.delete();
-    return message.reply("You have to wait 5 seconds between commands.")
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "8ball") {
+    var fortunes = [
+    "Yes",
+    "No",
+    "Maybe",
+    "Definitely!",
+    "Ask me later..",
+    "Fuck u",
+    "I'm not sure",
+    "Probably",
+    "I doubt..",
+    "Who? Me?",
+    "Sorry, I'm too lazy to answer",
+    "I thought you know the answer"
+    ];
+
+    var result = Math.floor((Math.random() * fortunes.length) + 0);
+
+    const embed = new Discord.RichEmbed()
+  .setColor(0xb798f2)
+  .setThumbnail("http://iconbug.com/data/95/256/8696325e0e7407823058632e68fb5970.png")
+  .setTitle("Results:")
+  .addField("Q:", args)
+  .addField("A:", fortunes[result]);
+  message.channel.send({embed});
+
   }
-  if(!message.member.hasPermission("ADMINISTRATOR")){
-    cooldown.add(message.author.id);
-  }
-
-
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
-  setTimeout(() => {
-    cooldown.delete(message.author.id)
-  }, cdseconds * 1000)
 
 });
 
-bot.on("guildMemberAdd", member => {
-  member.guild.channels.find(`name`, "test").send("Hi");
+client.on("guildMemberAdd", member => {
+  member.guild.channels.get("506563742352277507").send(member.user.toString() + " welcome to **ᴄ ʜ ɪ ʟ ʟ    ᴄ ᴏ ᴜ ɴ ᴛ ʏ**! Make sure to check out #welcome for any information, or ask a staff member! 💓 Enjoy your stay!");
 });
 
-bot.on("guildMemberRemove", member => {
-  member.guild.channels.find(`name`, "test").send("Bye");
+client.on("guildMemberRemove", member => {
+  member.guild.channels.get("506551891841253406").send("**" + member.user.username + "** " + leaveMessages[Math.floor(Math.random() * leaveMessages.length)]);
 });
 
-  bot.login(process.env.BOT_TOKEN);
+client.on("ready", () => {
+
+  client.user.setGame("| 𝒶𝑒𝓈𝓉𝒽𝑒𝓉𝒾𝒸")
+
+});
+
+client.login(process.env.BOT_TOKEN);
