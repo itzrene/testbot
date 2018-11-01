839ba582173c9cd23aacb4167081bf11d1d06468
itzrene <renko.krcmar12@gmail.com>
2018-11-01 18:25:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,42 @@ client.on("message", (message) => {
 
 });
 
+client.on("message", (message) => {
+
+  if(message.author.bot) return;
+  if(!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "pickle") {
+    var size = [
+    "1 inch",
+    "2 inch",
+    "3 inch",
+    "4 inch",
+    "5 inch",
+    "6 inch",
+    "7 inch",
+    "8 inch",
+    "9 inch",
+    "10 inch",
+    "11 inch",
+    "You don't have a pickle :("
+    ];
+
+    var result = Math.floor((Math.random() * size.length) + 0);
+
+    const embed = new Discord.RichEmbed()
+  .setColor(0xb798f2)
+  .setTitle("Results:")
+  .addField("Your pickle size is: ", size[result]);
+  message.channel.send({embed});
+
+  }
+
+});
+
 client.on("guildMemberAdd", member => {
   member.guild.channels.get("506563742352277507").send(member.user.toString() + " welcome to **ᴄ ʜ ɪ ʟ ʟ    ᴄ ᴏ ᴜ ɴ ᴛ ʏ**! Make sure to check out #welcome for any information, or ask a staff member! 💓 Enjoy your stay!");
 });
