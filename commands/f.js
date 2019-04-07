29bf90c0bda0d35822254b3cb90442fbb0af7d01
itzrene <renko.krcmar12@gmail.com>
2019-04-07 14:50:01 +0200

    Update f.js

--- commands/f.js
+++ commands/f.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!args[1]) return message.channel.send("**" + message.author.toString() + "** has paid their respects!");
+    if(!args[1]) return message.channel.send("**" + message.user.username + "** has paid their respects!");
 
     let prayers = args.slice(0).join(" ");
 
-    message.channel.send("**" + message.author.toString() + + "** has paid their respects for **" + prayers + "!**");
+    message.channel.send("**" + message.user.username + "** has paid their respects for **" + prayers + "!**");
 
 }
 
