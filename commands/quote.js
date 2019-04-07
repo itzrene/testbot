5efb1dd50640076bd324bda720f89262002a25ba
itzrene <renko.krcmar12@gmail.com>
2019-04-07 17:00:29 +0200

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -1,13 +1,8 @@
 const Discord = require("discord.js");
-const quotes = require("./quotes.json");
 
 module.exports.run = async (bot, message, args, member) => {
 
-    function randomQuote() {
-        return quotes[Math.floor(Math.random() * quotes.length)];
-    };
-
-    message.channel.send("*" + randomQuote() + "*");
+    message.channel.send("*");
 
 }
 
