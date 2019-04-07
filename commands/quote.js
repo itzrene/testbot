75bd97b68c46d83ea3730f3d767e9119218ff48d
itzrene <renko.krcmar12@gmail.com>
2019-04-07 17:35:49 +0200

    Create quote.js

---
+++ commands/quote.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+var config = require("/quotes.json");
+var quotes = config.quotes;
+
+
+module.exports.run = async (bot, message, args, member) => {
+
+    function randomQuote() {
+        return Math.floor((Math.random() * quotes.length));
+    };
+
+    message.channel.send(randomQuote());
+
+}
+
+module.exports.help = {
+    name: "quote"
+}
