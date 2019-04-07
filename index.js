6ca2f8c78e6026861d87405343ecbc387e4fc696
itzrene <renko.krcmar12@gmail.com>
2019-04-07 19:29:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -99,23 +99,6 @@ bot.on("message", function(message) {
 
 
 
-bot.on("message", function(message) {
-  if (message.content.toLowerCase() == "banner") {
-
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
-
-    // Since the image takes time to load, you should await it
-    const background = Canvas.loadImage('./wallpaper.jpg');
-    // This uses the canvas dimensions to stretch the image onto the entire canvas
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    // Use helpful Attachment class structure to process the file for you
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-    message.author.send("Banner created!", attachment);
-  }
-});
-
 
 
 
