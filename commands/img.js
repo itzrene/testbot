d3b5bf77b3a239581ba1a26ba2f56a817a17cb98
itzrene <renko.krcmar12@gmail.com>
2019-04-07 22:03:45 +0200

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -1,11 +1,16 @@
 const Discord = require("discord.js");
+var Jimp = require('jimp');
 
 module.exports.run = async (bot, message, args, member) => {
 
+    let image = await Jimp.read("images/wallpaper.jpg");
+
+    Jimp.read("images/wallpaper.jpg").then(image =>{
+        image.resizeBy(50,50)
+        image.write("wallpaper.jpg")
+    })
     
-
     message.channel.send("", {file: "images/wallpaper.jpg"});
-
 }
 
 module.exports.help = {
