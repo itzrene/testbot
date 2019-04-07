b4af08294035c82c9ada1b802a3b928eb29c837a
itzrene <renko.krcmar12@gmail.com>
2019-04-07 22:20:18 +0200

    Update img.js

--- commands/img.js
+++ commands/img.js
@@ -1,12 +1,29 @@
 const Discord = require("discord.js");
+var Jimp = require('jimp');
 
 module.exports.run = async (bot, message, args, member) => {
 
-    
-    
-    message.channel.send("", {file: "images/wallpaper.jpg"});
+    var p1 = Jimp.read("images/lenna.png");
+    var p2 = Jimp.read("images/mask.png");
+
+    Promise.all([p1, p2]).then(function(images){
+        var lenna = images[0];
+        var mask = images[1];
+
+        lenna.mask(mask, 0, 0).write("lenna-circle.png");
+
+        var sizes = [256, 128, 64, 60, 48, 32, 30, 16];
+
+        for (var i = 0; i < sizes.length; i++) {
+            lenna.clone().resize(sizes[i], Jimp.AUTO)
+                .write("lenna-circle" + i + ".png");
+        }
+    });
+
+    message.channel.send("", {file: "images/lenna-circle.png"});
 }
 
 module.exports.help = {
     name: "img"
 }
+
