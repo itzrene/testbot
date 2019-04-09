192e3d5e2d599989d8d26ecc0449d6f3036f6c60
itzrene <renko.krcmar12@gmail.com>
2019-04-09 14:36:38 +0200

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     
-    message.channel.send("Img sent!", {file: "images/meme/meme.png"});
+    message.channel.send("Img sent! ok", {file: "images/meme/meme.png"});
+    console.log("Meme!");
 
 }
 
