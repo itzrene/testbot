6aafeaa8f052b18ae97254f9cb0b59f4c82d89dd
itzrene <renko.krcmar12@gmail.com>
2019-04-14 00:47:08 +0200

    Create db.js

---
+++ commands/db.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const config = require("../botconfig.json");
+const db = require("../models/db.js");
+let color = config.color;
+
+module.exports.run = async (bot, message, args) => {
+
+    mongoose.connect(process.env.MONGODB);
+
+    const db = new Db({
+        _id: mongoose.Types.ObjectID(),
+        username: message.author.username,
+        arguments: args[0]
+    })
+
+    db.save()
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+
+    message.reply("Saved to the database!");
+
+}
+
+module.exports.help = {
+    name: "db"
+}
