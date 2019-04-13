f6319de04c3ce6b23deeb2fe7d161e3e4375a1a3
itzrene <renko.krcmar12@gmail.com>
2019-04-14 01:20:00 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const dbSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: String,
-    arguments: String
+    username: String
 });
 
 module.exports = mongoose.model("Db", dbSchema);
