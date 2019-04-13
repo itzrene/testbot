13cec281efc7ed39f889a2805dc8f48c5bac7a94
itzrene <renko.krcmar12@gmail.com>
2019-04-14 01:50:47 +0200

    Update db.js

--- commands/db.js
+++ commands/db.js
@@ -7,13 +7,21 @@ let color = config.color;
 module.exports.run = async (bot, message, args) => {
 
     mongoose.connect(process.env.MONGODB, {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        autoIndex: false, // Don't build indexes
+        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        reconnectInterval: 500, // Reconnect every 500ms
+        poolSize: 10, // Maintain up to 10 socket connections
+        // If not connected, return errors immediately rather than waiting for reconnect
+        bufferMaxEntries: 0,
+        connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        socketTimeoutMS: 45000,
+        family: 4 // Use IPv4, skip trying IPv6
     });
 
     const db = new Db({
         _id: mongoose.Types.ObjectId(),
-        username: message.author.username,
-        arguments: args[0]
+        username: message.author.username
     });
 
     db.save()
