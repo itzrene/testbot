65c02862c7445c2d415c90b6f78a550e9ced3fe5
itzrene <renko.krcmar12@gmail.com>
2019-04-15 16:48:53 +0200

    Update pray.js

--- commands/pray.js
+++ commands/pray.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("vedel som ze to skusis :herold:");
+  message.channel.send("vedel som ze to skusis <:Herold:567360511742574613>");
 
 }
 
