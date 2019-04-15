ca542d20550d93f41ddb04f71eab54508f6ae396
itzrene <renko.krcmar12@gmail.com>
2019-04-15 16:46:49 +0200

    Create pray.js

---
+++ commands/pray.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  message.channel.send("vedel som ze to skusis :herold:");
+
+}
+
+module.exports.help = {
+  name: "pray"
+}
