bb785a96b2c2f1f686f1ea038ccf0c7186a206d0
itzrene <renko.krcmar12@gmail.com>
2019-04-16 17:53:20 +0200

    Update fakebal.js

--- commands/fakebal.js
+++ commands/fakebal.js
@@ -15,8 +15,8 @@ const options = {
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
 };
-let uri = bitch;
-mongoose.connect(uri, options);
+let uri;
+mongoose.connect(process.env.MONGODB, options);
 
 module.exports.run = async (bot, message, args) => {
 
