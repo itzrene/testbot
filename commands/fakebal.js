f6076bd7e22f3647b8bb1c2c3014010b9badc69a
itzrene <renko.krcmar12@gmail.com>
2019-04-18 22:58:20 +0200

    Update fakebal.js

--- commands/fakebal.js
+++ commands/fakebal.js
@@ -13,7 +13,10 @@ const options = {
     bufferMaxEntries: 0,
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
+    family: 4, // Use IPv4, skip trying IPv6
+    host: '_host_',
+    port: 27017,
+    path: '/'
 };
 console.log(process.env.MONGODB_URI);
 let uri = process.env.MONGODB_URI;
