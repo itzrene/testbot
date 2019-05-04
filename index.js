cb7b5d1496a9022ed0c8ed3749ab0bf19c74ac9b
itzrene <renko.krcmar12@gmail.com>
2019-05-04 18:40:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("messageDelete", async message => {
     .addField("User:", message.author.tag, true)
     .addField("In:", message.channel, true)
     .addField("Message:", message.content)
-    .setColor(color); i
+    .setColor(color);
 
   let channel = message.guild.channels.find(x => x.name === 'magical-creature-logs');
   channel.send({embed});
