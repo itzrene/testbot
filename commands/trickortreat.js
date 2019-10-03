0ef3d30f8fb44fb4aea4b466b51bb679b67e12d5
itzrene <renko.krcmar12@gmail.com>
2019-10-03 23:50:05 +0200

    Create trickortreat.js

---
+++ commands/trickortreat.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let candies = Math.floor((Math.random() * fortunes.length));
+
+  let embed = new Discord.RichEmbed()
+  .setColor("0xEB6123")
+  .setThumbnail("https://mynorth.com/wp-content/uploads/2016/09/pumpkins-and-gourds-900x473.jpeg")
+  .setTitle("Happy halloween! 🎃:")
+  .addField("You got " + candies.toString() + " candies!", "🍫 🍬 🍬 🍭 🍫 🍬 🍭")
+
+  message.channel.send(embed);
+
+  bot.on("message", function(message) {
+    if (message.content.toLowerCase() == "happy halloween") {
+      message.channel.send("Hello! 💓 " + message.author.toString());
+    }
+  });
+
+  bot.on("message", function(message) {
+      if (message.content.toLowerCase() == "trick or treat") {
+        message.channel.send("Happy halloween! 🎃" + message.author.toString());
+      }
+    });
+
+    bot.on("message", function(message) {
+          if (message.content.toLowerCase() == "happy halloween") {
+            message.channel.send("Happy halloween! 🎃" + message.author.toString());
+          }
+        });
+}
+
+module.exports.help = {
+  name: "trickortreat"
+}
