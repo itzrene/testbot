6a80ea3aacec78d7a8dc11e14ccdb4ac48b3c54b
itzrene <renko.krcmar12@gmail.com>
2019-10-03 23:53:47 +0200

    Update trickortreat.js

--- commands/trickortreat.js
+++ commands/trickortreat.js
@@ -2,13 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  let candies = Math.floor((Math.random() * fortunes.length));
-
   let embed = new Discord.RichEmbed()
   .setColor("0xEB6123")
   .setThumbnail("https://mynorth.com/wp-content/uploads/2016/09/pumpkins-and-gourds-900x473.jpeg")
   .setTitle("Happy halloween! 🎃:")
-  .addField("You got " + candies.toString() + " candies!", "🍫 🍬 🍬 🍭 🍫 🍬 🍭")
+  .addField("You got 10 candies!", "🍫 🍬 🍬 🍭 🍫 🍬 🍭")
 
   message.channel.send(embed);
 
