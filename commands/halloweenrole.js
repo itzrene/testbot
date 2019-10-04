a6fea714770a6d23cf279052309103a3398d7573
itzrene <renko.krcmar12@gmail.com>
2019-10-05 00:40:27 +0200

    Create halloweenrole.js

---
+++ commands/halloweenrole.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(message.member.roles.find(role => role.name === "S p o o k y - v i b e s")){
+    message.channel.send(message.author.toString() + " You already have this role! 👻");
+    } else {
+    var role = message.guild.roles.find(role => role.name === "S p o o k y - v i b e s");
+    message.member.addRole(role);
+    message.channel.send(message.author.toString() + " I gave you the **S p o o k y - v i b e s** role! 🎃");
+    }
+
+}
+
+module.exports.help = {
+    name: "hr"
+}
