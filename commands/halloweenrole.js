dbf165322c457c31937920ee1c27f53cdb73736e
itzrene <renko.krcmar12@gmail.com>
2019-10-05 00:55:20 +0200

    Update halloweenrole.js

--- commands/halloweenrole.js
+++ commands/halloweenrole.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
     if(message.member.roles.find('name', 'S p o o k y - v i b e s')){
     message.channel.send(message.author.toString() + " You already have this role! 👻");
     } else {
-    const role = message.guild.roles.find('name', 'S p o o k y - v i b e s');
-    message.member.addRole(role);
+    let role = message.guild.roles.find('name', 'S p o o k y - v i b e s');
+    message.member.addRole(role.id);
     message.channel.send(message.author.toString() + " I gave you the **S p o o k y - v i b e s** role! 🎃");
     }
 
