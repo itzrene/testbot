ac350c6f6e49379b602bd2a9121e90023cd8cee9
itzrene <renko.krcmar12@gmail.com>
2019-10-05 16:08:36 +0200

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -3,7 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     let rated = message.mentions.members.first();
-    if(!rated) return message.channel.send("Tag someone to rate them!");
+    if(!rated) {
+        message.channel.send("Tag someone to rate them!");
+    } else {
     
     let rating = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     
@@ -11,8 +13,9 @@ module.exports.run = async (bot, message, args) => {
     
     if(rated.user.id === message.author.id) {
       message.channel.send(`**${message.author.username}**, I'd give you ${result}/10 <:engine2:569170469174116363>`);
-    } else {
-      message.channel.send(`I'd give **__${rated.user.username}__** ${result}/10 <:engine2:569170469174116363>`);
+    }   else {
+            message.channel.send(`I'd give **__${rated.user.username}__** ${result}/10 <:engine2:569170469174116363>`);
+        }
     }
     
 }
