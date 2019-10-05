c4e09b331d5469e0a5267ba7c4d1da92793c995f
itzrene <renko.krcmar12@gmail.com>
2019-10-05 14:57:15 +0200

    Update emojify.js

--- commands/emojify.js
+++ commands/emojify.js
@@ -2,39 +2,22 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, member) => {
 
-    function emojify(str) {
-    	if (typeof str === 'string') {
-    		return Array.prototype.map.call(str, (e, i, a) => {
-    			if (/[aA][bB]/.test(e+a[i+1])) {
-    				return ':ab:';
-    			} else if (/[oO]/.test(e)) {
-    				return ':o2:';
-    			} else if (/[aA]/.test(e)) {
-    				return ':a:';
-    			} else if (/[bB]/.test(e)) {
-    				if (/[aA]/.test(a[i-1])) {
-    					return;
-    				} else {
-    					return ':b:';
-    				}
-    			} else if (/[a-zA-Z]/.test(e)) {
-    				return ':regional_indicator_' + e.toLowerCase() + ':'
-    			} else {
-    				return e;
-    			}
-    		}).join('\u200C');
-    	} else {
-    		throw new TypeError('argument is not a string');
-    	}
-    }
+    const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>?@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
+    const OFFSET = '!'.charCodeAt(0);
     
-    if (args.length >= 0) {
-        emojify("hi");
+        if (args.length < 1) return message.channel.send("You must provide text to flip!");
+
+        message.channel.send(
+            args.join(' ').split('')
+                .map(c => c.charCodeAt(0) - OFFSET)
+                .map(c => mapping[c] || ' ')
+                .reverse().join('')
+        );
     }
 
 }
 
 module.exports.help = {
-    name: "emojify"
+    name: "flip"
 }
 
