5e90bcfcf0307d58f00ca3a796752de1b478fe00
itzrene <renko.krcmar12@gmail.com>
2019-10-05 02:08:12 +0200

    Update trickortreat.js

--- commands/trickortreat.js
+++ commands/trickortreat.js
@@ -2,11 +2,21 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
-  function candiesEmoji() {
+  /**function candiesEmoji() {
     var result = "";
     var characters = "🍫🍬🍭";
     var charactersLength = characters.length;
     for (var i = 0; i <= 7; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }**/
+  
+  function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
@@ -18,7 +28,7 @@ module.exports.run = async (bot, message, args) => {
   .setColor("0xEB6123")
   .setThumbnail("https://media2.s-nbcnews.com/j/newscms/2014_40/695336/141001-halloween-candy-corn-1700_539e2169509dc704b46829727907d138.fit-760w.jpg")
   .setTitle("Happy halloween! 🎃:")
-  .addField("You got " + candies.toString() + " candies!", candiesEmoji.toString())
+  .addField("You got " + candies.toString() + " candies!", makeid(7))
 
   message.channel.send(embed);
   
