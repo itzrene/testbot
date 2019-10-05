56c66d9911341b24ebb0814b9ff8e46b8cf572e3
itzrene <renko.krcmar12@gmail.com>
2019-10-05 14:59:19 +0200

    Update emojify.js

--- commands/emojify.js
+++ commands/emojify.js
@@ -5,15 +5,17 @@ module.exports.run = async (bot, message, args, member) => {
     const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>?@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
     const OFFSET = '!'.charCodeAt(0);
     
-        if (args.length < 1) return message.channel.send("You must provide text to flip!");
+        if (args.length < 1) {
+            return message.channel.send("You must provide text to flip!");
+        } else {
 
         message.channel.send(
             args.join(' ').split('')
                 .map(c => c.charCodeAt(0) - OFFSET)
                 .map(c => mapping[c] || ' ')
                 .reverse().join('')
-        );
-    }
+            );
+        } 
 
 }
 
