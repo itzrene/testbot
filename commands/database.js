2af9070eaa83f4f25766b64538ffd5a749e3182d
itzrene <renko.krcmar12@gmail.com>
2019-10-06 18:44:53 +0200

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -4,9 +4,26 @@ const config = require("../botconfig.json");
 let color = config.color;
 const mysql = require("mysql");
 
+let con = mysql.createConnection({
+    port: "3306",
+    host: "remotemysql.com",
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
+
+con.connect(err => {
+   if(err) {
+       //throw err;
+       console.log("fuck");
+   }
+   console.log("AAAAAAAAA Connected to the database of database.js!");
+   //con.query("SHOW TABLES", console.log);
+});
+
 module.exports.run = async (bot, message, args, con) => {
 
-    channel.send("1");
+    message.channel.send("1");
     con.query(`SELECT * FROM xp WHERE id = '${message.author.id}'`, (err, rows) => {
         if (err) {
             console.log("frick");
