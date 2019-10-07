361cb360bbf614b0518e29c45c0d5d2b040f9b88
itzrene <renko.krcmar12@gmail.com>
2019-10-07 16:14:54 +0200

    Update candy.js

--- commands/candy.js
+++ commands/candy.js
@@ -4,9 +4,9 @@ var DB = require('../DB.js');
 module.exports.run = async (bot, message, args) => {
   
   DB.query(`SELECT * FROM candies WHERE id = '${message.author.id}'`, (err, result) => {
-    if (err) {
+    /**if (err) {
       console.log("CANDY PROBLEM!!!);
-    }
+    }**/
     let candy = result[0].candy;
     message.channel.send(message.author.toString() + ", you have **" + candy.toString() + "** candies! 🍬 🍭");
   });
