0e11c34498fff5104bef3cfa78f434ceba3ace52
itzrene <renko.krcmar12@gmail.com>
2019-10-07 20:16:23 +0200

    Update currency.js

--- commands/currency/currency.js
+++ commands/currency/currency.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
-const db = require("quick.db");
 const config = require("../../botconfig.json");
+var DB = require("../DB.js");
 let color = config.color;
 
 module.exports.run = async (bot, message, args) => {
@@ -31,7 +31,7 @@ module.exports.run = async (bot, message, args) => {
     }
      **/
 
-    if(args[0]){
+    /**if(args[0]){
         let target = message.mentions.members.first();
         let targetBal = db.fetch(`currency_${target.id}`);
 
@@ -54,8 +54,42 @@ module.exports.run = async (bot, message, args) => {
             .setDescription("Your balance is `" + bal + "` 🍵!")
             .setColor(color)
         message.channel.send(embed)
-    }
-
+    }**/
+    
+    
+        let target = message.mentions.members.first();
+    
+        if(args[0]){
+            DB.query(`SELECT * FROM currency WHERE id = '${target.id}'`, (err, result) => {
+                 if(result.length < 1) { 
+                    message.channel.send(target.username + ", doesn't have any 🍵!");
+                 } else {
+                    let targetBal = result[0].bal;
+            
+                    let embed = new Discord.RichEmbed()
+                        .setDescription(target.username + "'s balance is `" + targetBal + "` 🍵!")
+                        .setColor(color)
+                    message.channel.send(embed);
+                    console.log("TARGET HAS SOME MONEY");
+                }
+           });
+        } else {
+            DB.query(`SELECT * FROM currency WHERE id = '${message.author.id}'`, (err, result) => {
+                
+                if(result.length < 1) { 
+                    message.channel.send(message.author.toString() + ", you don't have any 🍵!");
+                } else {
+                    let bal = result[0].bal;
+            
+                    let embed = new Discord.RichEmbed()
+                        .setDescription("Your balance is `" + bal + "` 🍵!")
+                        .setColor(color)
+                    message.channel.send(embed);
+                    console.log("HAS SOME MONEY");
+                }
+                
+           });
+        }
 }
 
 module.exports.help = {
