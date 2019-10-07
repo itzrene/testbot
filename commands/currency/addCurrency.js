d2642ca901918b022463187dfe21c53b5e95684e
itzrene <renko.krcmar12@gmail.com>
2019-10-07 20:36:32 +0200

    Update addCurrency.js

--- commands/currency/addCurrency.js
+++ commands/currency/addCurrency.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
-const db = require("quick.db");
 const config = require("../../botconfig.json");
+var DB = require("../../DB.js");
 let color = config.color;
 
 module.exports.run = async (bot, message, args) => {
-
+    
     if (!message.member.hasPermission('ADMINISTRATOR')) {
         let embed = new Discord.RichEmbed()
             .setAuthor("ERROR", message.author.avatarURL)
@@ -28,13 +28,23 @@ module.exports.run = async (bot, message, args) => {
                     .setColor(color)
                 message.channel.send(embed).then(msg => msg.delete(5000));
             } else {
-                db.add(`currency_${target.id}`, args[1]);
+                let balToAdd = args[1];
+                var sql;
+                DB.query(`SELECT * FROM currency WHERE id = '${target.id}'`, (err, result) => {
+                    if(result.length < 1){
+                        sql = `INSERT INTO currency (id, bal) VALUES ('${target.id}', ${balToAdd})`;
+                    } else {
+                        let currBal = result[0].bal;
+                        sql = `UPDATE currency SET bal = ${currBal + balToAdd} WHERE id = '${target.id}'`;
 
-                let embed = new Discord.RichEmbed()
-                    .setDescription(target.user + " `has been awarded " + args[1] + "` 🍵`!`")
-                    .setColor(color)
+                        let embed = new Discord.RichEmbed()
+                            .setDescription(target.user + " `has been awarded " + args[1] + "` 🍵`!`")
+                            .setColor(color)
 
-                message.channel.send(embed).then(m => m.delete(5000));
+                        message.channel.send(embed).then(m => m.delete(5000));
+                    }
+                    DB.query(sql, "ADDED RECORD CURRENCY");
+                });
             }
         }
     }
