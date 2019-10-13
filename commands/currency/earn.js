8ad262f501520f4d586de447b47c7b771803668d
itzrene <renko.krcmar12@gmail.com>
2019-10-13 22:31:59 +0200

    Update earn.js

--- commands/currency/earn.js
+++ commands/currency/earn.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const config = require("../../botconfig.json");
 var DB = require("../../DB.js");
 let color = config.color;
