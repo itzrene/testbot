664af73f7497f0de0f05467e54bcf1140f2909b8
itzrene <renko.krcmar12@gmail.com>
2019-10-25 22:03:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");k
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const fs = require("fs");
 const botconfig = require("./botconfig.json");
