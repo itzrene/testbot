6d71c29283a0649c727ca989e0c57aff9c326671
itzrene <renko.krcmar12@gmail.com>
2020-10-11 22:51:50 +0200

    Update DB.js

--- DB.js
+++ DB.js
@@ -1,1 +1,57 @@
+const mysql = require("mysql");
 
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host: "remotemysql.com",
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+});
+
+
+var DB = (function () {
+
+    function _query(query, params, callback) {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                connection.release();
+                //callback(null, err);
+                console.log("PROBLEM HOUSTON!!!")
+                //throw err;
+            }
+
+            connection.query(query, params, function (err, rows) {
+                connection.release();
+                if (!err) {
+                    //callback(rows);
+                    //console.log("PROBLEM HOUSTON!!! 2")
+                    //throw err;
+                }
+                else {
+                    //callback(null, err);
+                    //console.log("PROBLEM HOUSTON!!! 3")
+                }
+
+            });
+
+            connection.on('error', function (err) {
+                connection.release();
+                //callback(null, err);
+                //throw err;
+                console.log("PROBLEM HOUSTON!!! ANOTHER")
+                console.log(err);
+            });
+        });
+    };
+
+    return {
+        query: _query
+    };
+})();
+
+DB.query("SELECT * FROM customers", function (data, error) {
+   //callback(data, error);
+   console.log("SELECTED");
+});
+
+module.exports = DB;
